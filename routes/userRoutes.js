79b4ab5d25bb2c8da0b72df008c4ba4ed836a949
ayo baj <ayobaj007@gmail.com>
2024-03-19 09:43:16 +0100

    implemeted the neccessary methods

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,30 +1,66 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/user');
+const user = require('../models/user');
 
 // TO GET ALL THE USERS
 router.get('/', async (req, res) => {
     try {
-        const users = await User.find();
+        const users = await user.find();
         res.json(users);
     } catch (err) {
         res.status(500).json({ message: err.message });
     }
 });
 
-// TO POST A NEW USER
+// THE POST METHOD FOR A NEW USER
 router.post('/', async (req, res) => {
-    
-});
+    const { name, email } = req.body;
+    try {
+        const newUser = new user({ name, email });
+        await newUser.save();
+        res.status(201).json(newUser);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+    });
+
 
 // PUT A NEW USER USING AN ID
 router.put('/:id', async (req, res) => {
-    
-});
+    const { id } = req.params;
+    const { name, email } = req.body;
+
+    try {
+        const updatedUser = await User.findByIdAndUpdate(id, { name, email }, { new: true });
+        if (!updatedUser) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        res.json(updatedUser);
+        } catch (err) {
+        res.status(400).json({ message: err.message });
+        }
+    });
+
 
 // DELTE A USER USING AN ID
+
 router.delete('/:id', async (req, res) => {
-    
+    const { id } = req.params;
+
+    try {
+
+        const deletedUser = await user.findByIdAndDelete(id);
+
+    if (!deletedUser) {
+        return res.status(404).json({ message: 'User not found' });
+    }
+
+    res.json({ message: 'User deleted successfully' });
+    } catch (err) {
+      // Handle errors
+    res.status(500).json({ message: err.message });
+    }
 });
+
 
 module.exports = router;
