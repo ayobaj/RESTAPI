946d3d3bb9a56ce212fc254aff56e026cf84af0a
ayo baj <ayobaj007@gmail.com>
2024-03-19 15:54:58 +0100

    added the mongo URI as a string due to connection error

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true,})
+mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://aybajulaye007:gjstNEvAUYtiLSm@<cluster-url>?retryWrites=true&writeConcern=majority', { useNewUrlParser: true, useUnifiedTopology: true,})
 
 .then(()=> {
     console.log('mongodb connected')
